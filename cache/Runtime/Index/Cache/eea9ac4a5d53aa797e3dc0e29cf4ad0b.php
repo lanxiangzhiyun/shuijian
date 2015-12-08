e764dafa6c003dcc02c803f99200e684ff374cf6
lanxianghzhiyun <fanghui_net_cslg@126.com>
2015-12-08 18:33:07 +0800

    20151208

---
+++ cache/Runtime/Index/Cache/eea9ac4a5d53aa797e3dc0e29cf4ad0b.php
@@ -0,0 +1,288 @@
+<?php if (!defined('THINK_PATH')) exit();?><!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head>
+  <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+  <meta content="telephone=no" name="format-detection" />
+  <meta name="keywords" content="HeyJuice,轻断食果汁,纤体,冷榨蔬果汁,轻断食蔬果汁,HeyJuice轻断食蔬果汁,果汁,果蔬汁,轻断食果蔬汁,冷榨果汁" />
+  <meta name="description" content="轻断食果蔬汁HeyJuice，3天变美计划。营养学家设计的冷压鲜榨果蔬汁餐单，每天6瓶连续3天，排出体内毒素，激发细胞修复力，减去多余脂肪、美颜、身体变轻盈，充满青春活力！新鲜水果与蔬菜冷压鲜榨，全程冷链免费送达。" />
+  <title>轻断食果蔬汁HeyJuice，3天变美计划</title>
+  <script type="text/javascript" src="/Public/Common/js/base.js?v=1.3"></script>
+  <script type="text/javascript" src="/Public/Common/js/common.js?v=1.3"></script>
+  <script type="text/javascript" src="/Public/Home/js/global.js?v=1.3"></script>
+  <script type="text/javascript" src="/Public/Common/js/jquery.lazyload.min.js?v=1.3"></script>
+  <script type="text/javascript">
+    var NTKF_PARAM = {
+      siteid:"kf_9700",
+      settingid:"kf_9700_1410331097630",
+      itemid:"",
+      uid:"119641",
+      uname:"用户3ISGXT99",
+      orderid:"<",
+      orderprice:"",
+      userlevel:""
+    }
+  </script>
+  <script type="text/javascript" src="http://download.ntalker.com/t2d2/ntkfstat.js?" charset="utf-8"></script>
+  <link rel="stylesheet" type="text/css" href="/Public/Home/css/global.css?v=1.3">
+  <link rel="stylesheet" type="text/css" href="/Public/Common/css/base.css?v=1.3">
+</head>
+<body>
+<div class="wrap Wrap">
+<div class="navtop">
+  <div class="navtop_box">
+    <div class="user_info_bar">
+      <div class="user_info_bar_icon HeaderUserBtn">
+        <img src="/Public/Common/images/default_avatar.png" width="28px" height="28px">
+      </div>
+      <div class="user_info_bar_list clearfix HeaderUserList">
+        <ul>
+          <li><a href="/Customer/info">我的资料</a></li>
+          <li><a href="/Customer/order">我的订单</a></li>
+          <li><a href="/Customer/address">地址管理</a></li>
+          <li><a href="/Customer/virtual">抵用金</a></li>
+          <li><a href="/Customer/logout">退出登录</a></li>
+        </ul>
+      </div>
+    </div>
+    <div class="navtop_info">为更好的自己，Better Me!</div>
+  </div>
+</div>
+<div class="header ">
+  <div class="heard_warp clearfix">
+    <div class="logo">
+      <a href="/"></a>
+      <img src="/Public/Home/images/re_index_logo.png"/ width="70px">
+    </div>
+    <div class="header_bar HeaderBar">
+      <ul>
+        <li><a class="" href="/Index">首页</a></li>
+        <li><a class="" href="/Product">产品</a></li>
+        <li><a class="" href="/Comments/index">顾客口碑</a></li>
+        <li><a class="" href="/Brand">品牌活动</a></li>
+        <li><a class="" href="/Service">品牌理念</a></li>
+      </ul>
+      <div class="shoping_car">
+        <a class="ShoppingCarLink" href="/ShoppingCar">
+        </a>
+      </div>
+    </div>
+
+  </div>
+</div>
+<div class="main">
+
+  <div class="information_main">
+    <div class="user_info_box">
+      <div class="info_tittle">帐号设置</div>
+      <div class="info_list">
+        <table width="100%;">
+          <tr class="ctr_user_info">
+            <th>个人资料</th>
+            <th></th>
+            <th></th>
+          </tr>
+          <tr>
+            <td colspan="3">
+              <div class="ctr_set_info">
+                <div class="ctr_set_input">
+                  &nbsp;&nbsp;&nbsp;&nbsp;姓名：<input type="text" class="InfoEditValue" name="customer_name" value="用户3ISGXT99" placeholder="请输入昵称">
+                </div>
+                <div class="ctr_set_input">
+                  &nbsp;&nbsp;&nbsp;&nbsp;性别：<label class="GenderSelect sex_radio "><input class="InfoEditValue"  value="1" type="radio" name="gender">男</label><label class="GenderSelect sex_radio "><input class="InfoEditValue"  value="2" type="radio"  name="gender">女</label>
+                </div>
+                <div class="ctr_info_save InfoEditSubmit">保存</div>
+              </div>
+            </td>
+          </tr>
+          <tr class="ctr_info_icon">
+            <th>头像</th>
+            <th></th>
+            <th></th>
+          </tr>
+          <tr class="ctr_set_icon">
+            <td colspan="3" >
+              <div class="set_user_icon"><img src="/Public/Common/images/default_avatar.png" width="50px"></div>
+              <div class="update_icon AvatarEditFile"></div>
+            </td>
+          </tr>
+          <tr class="ctr_user_pass">
+            <th>修改密码</th>
+            <th></th>
+            <th></th>
+          </tr>
+          <tr class="ctr_set_password">
+            <td colspan="3">
+              <div class="ctr_set_input">
+                旧密码：
+                <input type="password" class="PwdEditValue" name="password_old" placeholder="输入旧密码">
+              </div>
+              <div class="ctr_set_input">
+                新密码：
+                <input type="password" class="PwdEditValue" name="password" placeholder="输入新密码">
+              </div>
+              <div class="ctr_set_input _mr">
+                确认新密码：
+                <input type="password" class="PwdEditValue" name="password_repeat" placeholder="确认新密码">
+              </div>
+              <div class="ctr_info_save PwdEditSubmit">保存</div>
+            </td>
+          </tr>
+          <tr class="ctr_user_pass">
+            <th>绑定微信</th>
+            <th></th>
+            <th></th>
+          </tr>
+          <tr class="ctr_set_password">
+            <td colspan="3" style="text-aglin:center">
+              <div class="wechat_eq">
+                <img src="/Uploads/qrcode/1448432872119641.png" alt="">
+              </div>
+            </td>
+          </tr>
+        </table>
+      </div>
+    </div>
+  </div>
+  <script type="text/javascript">
+    $(function(){
+      Common.Pull('/Customer/wechat_bind_status',{},3000);
+    });
+  </script>
+  <script type="text/javascript">
+    $(function(){
+      var BindWechat = (function(){
+        return {
+          cancel : function(){
+            Base.Http.ajax({
+              url : '/Customer/cancel_wechat_bind',
+              success : function(data){
+                if (data['status']==1) {
+                  window.location.reload();
+                }else{
+                  Common.Prompt.alert({
+                    tips : data['info'],
+                    enter_text : '确认'
+                  });
+                }
+              }
+            });
+          }
+        }
+      }());
+      $('.CancelWechatBind').click(BindWechat.cancel);
+      Base.Form.upload({
+        object : $('.AvatarEditFile'),
+        target : 'avatar',
+        action : '/Customer/avatar_submit',
+        name : 'avatar',
+        id : 'avatar'
+      });
+      var Customer = {
+        info : {
+          submit : function(){
+            var data = Base.Form.serialize($('.InfoEditValue'));
+            Base.Http.ajax({
+              url : '/Customer/info_submit',
+              data : data,
+              success : function(data){
+                Common.Prompt.auto({
+                  tips : data['info']
+                });
+                if (data['status']==1) {
+                  window.location.reload();
+                }
+              }
+            });
+          },
+          gender : function(){
+            $(this).addClass('this').siblings('label').removeClass('this');
+          }
+        },
+        pwd : {
+          submit : function(){
+            var data = Base.Form.serialize($('.PwdEditValue'));
+            Base.Http.ajax({
+              url : '/Customer/pwd_submit',
+              data : data,
+              success : function(data){
+                Common.Prompt.auto({
+                  tips : data['info']
+                });
+                if (data['status']==1) {
+                  window.location.reload();
+                }
+              }
+            });
+          }
+        }
+      };
+      $('.GenderSelect').click(Customer.info.gender);
+      $('.InfoEditSubmit').click(Customer.info.submit);
+      $('.PwdEditSubmit').click(Customer.pwd.submit);
+    });
+  </script>
+</div>
+<div class="footer">
+  <div class="footer_logo">
+    <embed src="/Public/Home/images/footer_logo.svg" type="image/svg+xml" width="280px" />
+  </div>
+  <div class="footer_info">
+    <div class="eq_code">
+      <img src="/Public/Home/images/eqcode.png">
+      <p>关注微信<br>关注更多活动信息</p>
+    </div>
+    <div class="about_we">
+      <p class="about_h1">联系HeyJuice获得在线支持</p>
+      <p class="f12">7x13小时专心服务,请通过任意方式联系我们</p>
+      <ul class="iconfont">
+        <li><a href="javascript:;" onclick="NTKF.im_openInPageChat()" class="icon-0001"></a>
+        <li><a href="http://weibo.com/HeyJuiceCleanse" class="icon-0003"></a></li>
+        <li><a href="http://wpa.qq.com/msgrd?v=3&uin=892280077&site=qq&menu=yes" class="icon-0002"></a></li>
+        <li><a href="javascript:;" title="4006-123-666" class="icon-0004"></a></li>
+      </ul>
+      <p>010-60645096（9：00－22：00）</p>
+      <p>4006-123-666（9：00－22：00）</p>
+    </div>
+    <div class="better_me">
+      为更好的自己<br>Better Me!
+    </div>
+  </div>
+</div>
+<div class="friendly_link">
+  <ul>
+    <li><a href="/About/index">关于HeyJuice</a></li>
+    <li><a href="/About/joinus">加入我们</a></li>
+    <li><a href="/About/contact">联系我们</a></li>
+    <li><a href="/Qa/index">帮助中心</a></li>
+    <li><a href="/About/contact#bd">商务合作</a></li>
+    <li><a href="/Brand/index">品牌活动</a></li>
+    <li><a href="/Delivery/index">配送范围查询</a></li>
+  </ul>
+  <div class="f12 mt30 mb10" style="width:950px; margin-left:auto; margin-right:auto;">
+    <table width="100%">
+      <tr>
+        <td>北京和聚网络科技有限公司&nbsp&nbsp&nbsp&nbsp京ICP备14009743号 ©HeyJuice 2014&nbsp&nbsp&nbsp&nbsp北京市朝阳区东三环北路丙2号18层21B06 </td>
+        <td rowspan="2"><img src="/Public/Home/images/qs.png"></td>
+      </tr>
+      <tr>
+        <td>营业执照注册号：110108016581054&nbsp&nbsp&nbsp&nbsp食品流通许可证编号：SP1101051410304043(1-1)&nbsp&nbsp&nbsp&nbsp产品标准号：GB/T31121&nbsp&nbsp&nbsp&nbsp生产许可证：QS1127 0601 0435</td>
+      </tr>
+    </table>
+  </div>
+</div>
+</div>
+<!-- 弹出层 -->
+<div class="mask_pro MarkWrap"></div>
+<div style="display:none;">
+  <script type="text/javascript">
+    var _bdhmProtocol = (("https:" == document.location.protocol) ? " https://":" http://");
+    document.write(unescape("%3Cscript src='" + _bdhmProtocol + "hm.baidu.com/h.js%3Fe1e9293140df80038214b7e8ebdc4ab7' type='text/javascript'%3E%3C/script%3E"));
+  </script>
+</div>
+<script type="text/javascript" charset="utf-8">
+  $(function() {
+    $("img.lazy").lazyload({effect: "fadeIn"});
+  });
+</script>
+</body>
+</html>
